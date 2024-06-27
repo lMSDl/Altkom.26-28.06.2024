b93a6381134fa92f2b52285c35dac90acc620c38
Paweł Łęczycki <pawel@msd.info.pl>
2024-06-27 14:27:23 +0200

    HttpClient

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,57 @@
+﻿
+
+using ConsoleApp;
+using Models;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+var httpClient = new HttpClient();
+httpClient.DefaultRequestHeaders.Accept.Add(MediaTypeWithQualityHeaderValue.Parse("application/json"));
+httpClient.BaseAddress = new Uri("http://localhost:5087/api/");
+
+
+var response = await httpClient.GetAsync("shoppingLists");
+
+/*if(response.StatusCode != System.Net.HttpStatusCode.OK)
+{
+    Console.WriteLine($"Status not OK ({response.StatusCode})");
+    return;
+}*/
+
+/*if(!response.IsSuccessStatusCode)
+{
+    Console.WriteLine($"Status not success ({response.StatusCode})");
+    return;
+}*/
+
+response.EnsureSuccessStatusCode();
+
+var options = new JsonSerializerOptions() { ReferenceHandler = ReferenceHandler.Preserve, PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+var shoppingLists = await response.Content.ReadFromJsonAsync < IEnumerable < ShoppingList >> (options);
+
+foreach (var item in shoppingLists)
+{
+    Console.WriteLine(item.Name);
+}
+
+
+var product = new Product() { Name = "Toy", Price = 12.2f };
+
+response = await httpClient.PostAsJsonAsync($"shoppingLists/{shoppingLists.First().Id}/Products", product, options);
+
+response.EnsureSuccessStatusCode();
+
+product = await response.Content.ReadFromJsonAsync<Product>(options);
+
+Console.WriteLine(product.Id);
+
+var webApiClient = new WebApiClient("http://localhost:5087/api/");
+
+product = await webApiClient.GetAsync<Product>($"Products/{product.Id}");
+
+Console.WriteLine(  product.Name);
+
+
+Console.ReadLine();
---
+++ ConsoleApp/WebApiClient.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class WebApiClient : IDisposable
+    {
+        private HttpClient _client;
+        private JsonSerializerOptions JsonSerializerSettings { get; set; } = new JsonSerializerOptions() { ReferenceHandler = ReferenceHandler.Preserve, PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+
+        public WebApiClient(string baseAddress)
+        {
+            _client = new HttpClient()
+            {
+                BaseAddress = new Uri(baseAddress)
+            };
+            _client.DefaultRequestHeaders.Accept.Add(MediaTypeWithQualityHeaderValue.Parse("application/json"));
+        }
+
+        public void Dispose()
+        {
+            _client.Dispose();
+        }
+        public async Task<T?> GetAsync<T>(string request)
+        {
+            var response = await _client.GetAsync(request);
+            //response.EnsureSuccessStatusCode();
+            if (!response.IsSuccessStatusCode)
+                return default;
+
+            return await response.Content.ReadFromJsonAsync<T?>();
+        }
+        public async Task<T?> PostAsync<T>(string request, T payload)
+        {
+            var response = await _client.PostAsJsonAsync(request, payload);
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadFromJsonAsync<T?>();
+        }
+
+
+    }
+}
